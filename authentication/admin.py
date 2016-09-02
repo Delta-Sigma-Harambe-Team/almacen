d3d4c6a63650ef0e8c2410f938a85e90b515af43
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-01 21:36:37 -0500

    implemented settings view, not working, it is even letal to use

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Account
 # Register your models here.
 @admin.register(Account)
-class AdminDuck(admin.ModelAdmin):
-    list_display = ('first_name','last_name', 'username', 'email', 'is_admin',\
+class AdminAccount(admin.ModelAdmin):
+    list_display = ('username', 'email','first_name','last_name', 'is_admin',\
     	'created_at','updated_at')
     list_filter = ('username','email')
