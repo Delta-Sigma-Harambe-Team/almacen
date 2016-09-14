ebca5cd31818d2e775753e657eabfccba989d895
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-13 20:49:39 -0500

    missing to add urls on project

--- almacen/urls.py
+++ almacen/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include('authentication.urls', namespace='auth')),
-    url(r'^', include('posts.urls', namespace='auth')), 
+    url(r'^', include('posts.urls', namespace='posts')),
+	url(r'^', include('products.urls', namespace='products')),     
 
     url('^.*$', IndexView.as_view(), name='index'),
 ]
