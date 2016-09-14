8f03958dc7d2be9525c0e2142d70bcd92d741ea3
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-13 21:00:15 -0500

    fixed models issue on max decimals

--- products/models.py
+++ products/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Resource(models.Model):
     name   = models.CharField(max_length=140, blank=False)          
-    amount = models.DecimalField(max_digits = 4, decimal_places=2, blank=False) #Total en gramos
-    price  = models.DecimalField(max_digits = 4, decimal_places=2, blank=False) #Precio por KiloGramo
+    amount = models.DecimalField(max_digits = 10, decimal_places=2, blank=False) #Total en gramos
+    price  = models.DecimalField(max_digits = 10, decimal_places=2, blank=False) #Precio por KiloGramo
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    due_date   = models.DateTimeField(blank = True)
+    due_date   = models.DateTimeField(blank = True)     #Debe ser mayor a la fecha en la que se crea
 
     def __unicode__(self):
         return '{0}'.format(self.content)
