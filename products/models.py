a684f4b7c5f7bf319bce6261b7e30cf0a155fda8
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-13 20:44:36 -0500

    fixed ident issue

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Resource(models.Model):
-	name   = models.CharField(max_length=140, blank=False)			
-    amount = models.DecimalField(max_digits = 4, decimal_places=2, blank=False)	#Total en gramos
+    name   = models.CharField(max_length=140, blank=False)          
+    amount = models.DecimalField(max_digits = 4, decimal_places=2, blank=False) #Total en gramos
     price  = models.DecimalField(max_digits = 4, decimal_places=2, blank=False) #Precio por KiloGramo
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,4 +15,4 @@ class Resource(models.Model):
         return '{0}'.format(self.content)
 
     def __unicode__(self):
-    	return '%s En Stock: %sg $%s/Kg'%(self.name,self.amount,self.price)
+        return '%s En Stock: %sg $%s/Kg'%(self.name,self.amount,self.price)
