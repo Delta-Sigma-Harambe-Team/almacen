39c4efd7ea783df665b5c0cda80cb12a1f617d79
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-18 15:08:44 -0500

    added inlines to django admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
+
+class TermInlineAdmin(admin.StackedInline): # TabularInline
+    model = Order.item.through
+    extra=0
 # Register your models here.
 @admin.register(Restaurant)
 class AdminRestaurant(admin.ModelAdmin):
@@ -10,8 +14,11 @@ class AdminRestaurant(admin.ModelAdmin):
 class AdminOrder(admin.ModelAdmin):
     list_display = ('requester', 'created_at','updated_at')
     list_filter = ('requester',)
+    inlines = (TermInlineAdmin,)
 
+'''
 @admin.register(OrderItem)
 class AdminOrderItem(admin.ModelAdmin):
     list_display = ('order', 'item','amount')
     list_filter = ('order','amount')
+'''
