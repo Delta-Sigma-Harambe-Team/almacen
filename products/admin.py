e0e0d7e9b70b364bacd80a9ba86611f5c9a91b46
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-09-23 15:56:48 -0500

    Updated icons :tada:

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,6 @@ from .models import Resource
 # Register your models here.
 @admin.register(Resource)
 class AdminResource(admin.ModelAdmin):
+    icon = '<i class="material-icons">shopping_basket</i>'
     list_display = ('name','amount','price','due_date')
     list_filter = ('name','amount','due_date')
--- restaurants/admin.py
+++ restaurants/admin.py
@@ -5,18 +5,21 @@ class TermInlineAdmin(admin.StackedInline): # TabularInline
     model = Order.item.through
     extra=0
 # Register your models here.
+@admin.register(OrderItem)
+class AdminOrderItem(admin.ModelAdmin):
+    icon = '<i class="material-icons">code</i>'
+    list_display = ('order', 'item','amount')
+    list_filter = ('order','amount')
+    
 @admin.register(Restaurant)
 class AdminRestaurant(admin.ModelAdmin):
+    icon = '<i class="material-icons">store</i>'
     list_display = ('name','address', 'created_at','updated_at')
     list_filter = ('name',)
 
 @admin.register(Order)
 class AdminOrder(admin.ModelAdmin):
+    icon = '<i class="material-icons">content_paste</i>'
     list_display = ('requester','status' ,'created_at','updated_at')
     list_filter = ('requester',)
     inlines = (TermInlineAdmin,)
-
-@admin.register(OrderItem)
-class AdminOrderItem(admin.ModelAdmin):
-    list_display = ('order', 'item','amount')
-    list_filter = ('order','amount')
